2ebdfedaa868c501ad378e81137f6c05c94c483e
JC <blahblahblah@email.com>
2020-02-06 10:32:29 -0600

    Add project files.

---
+++ HouseplantSuggestions/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace HouseplantSuggestions
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chkSouthFacing = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trkTemp = new System.Windows.Forms.TrackBar();
+            this.lblTemp = new System.Windows.Forms.Label();
+            this.btnSuggest = new System.Windows.Forms.Button();
+            this.lblSuggestion = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trkTemp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chkSouthFacing
+            // 
+            this.chkSouthFacing.AutoSize = true;
+            this.chkSouthFacing.Location = new System.Drawing.Point(51, 51);
+            this.chkSouthFacing.Name = "chkSouthFacing";
+            this.chkSouthFacing.Size = new System.Drawing.Size(202, 17);
+            this.chkSouthFacing.TabIndex = 0;
+            this.chkSouthFacing.Text = "Do you have a south-facing window?";
+            this.chkSouthFacing.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "How warm is your home?";
+            // 
+            // trkTemp
+            // 
+            this.trkTemp.Location = new System.Drawing.Point(58, 176);
+            this.trkTemp.Maximum = 90;
+            this.trkTemp.Minimum = 50;
+            this.trkTemp.Name = "trkTemp";
+            this.trkTemp.Size = new System.Drawing.Size(324, 45);
+            this.trkTemp.TabIndex = 2;
+            this.trkTemp.Value = 50;
+            this.trkTemp.Scroll += new System.EventHandler(this.trkTemp_Scroll);
+            // 
+            // lblTemp
+            // 
+            this.lblTemp.AutoSize = true;
+            this.lblTemp.Location = new System.Drawing.Point(414, 191);
+            this.lblTemp.Name = "lblTemp";
+            this.lblTemp.Size = new System.Drawing.Size(32, 13);
+            this.lblTemp.TabIndex = 3;
+            this.lblTemp.Text = "50 °F";
+            // 
+            // btnSuggest
+            // 
+            this.btnSuggest.Location = new System.Drawing.Point(74, 264);
+            this.btnSuggest.Name = "btnSuggest";
+            this.btnSuggest.Size = new System.Drawing.Size(75, 23);
+            this.btnSuggest.TabIndex = 4;
+            this.btnSuggest.Text = "Suggest!";
+            this.btnSuggest.UseVisualStyleBackColor = true;
+            // 
+            // lblSuggestion
+            // 
+            this.lblSuggestion.AutoSize = true;
+            this.lblSuggestion.Location = new System.Drawing.Point(215, 274);
+            this.lblSuggestion.Name = "lblSuggestion";
+            this.lblSuggestion.Size = new System.Drawing.Size(109, 13);
+            this.lblSuggestion.TabIndex = 5;
+            this.lblSuggestion.Text = "Plant suggestion here";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 355);
+            this.Controls.Add(this.lblSuggestion);
+            this.Controls.Add(this.btnSuggest);
+            this.Controls.Add(this.lblTemp);
+            this.Controls.Add(this.trkTemp);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chkSouthFacing);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trkTemp)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chkSouthFacing;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trkTemp;
+        private System.Windows.Forms.Label lblTemp;
+        private System.Windows.Forms.Button btnSuggest;
+        private System.Windows.Forms.Label lblSuggestion;
+    }
+}
+
---
+++ HouseplantSuggestions/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HouseplantSuggestions
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void trkTemp_Scroll(object sender, EventArgs e)
+        {
+            //takes track value, turns into string type
+            //sets temp text equal to track string value
+            lblTemp.Text = trkTemp.Value.ToString("# °F");
+        }
+    }
+}
